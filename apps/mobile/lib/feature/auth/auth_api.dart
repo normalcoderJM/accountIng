fdb2dabee9217c429bbff960e5b9627dee7c98d8
js-eugene <eugene@gmail.com>
2026-07-28 22:56:27 +0800

    fix: test auth

--- apps/mobile/lib/feature/auth/auth_api.dart
+++ apps/mobile/lib/feature/auth/auth_api.dart
@@ -14,6 +14,7 @@ class AuthApi {
     required String password,
   }) async {
     final uri = Uri.parse('$baseUrl/api/v1/auth/register');
+    print("url");
     final response = await http.post(
       uri,
       headers: {'Content-Type': 'application/json'},
